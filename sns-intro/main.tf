262966b67368fc77f759e1a13e92eb63be8f34e6
rteles86 <rteles@outlook.com>
2026-02-08 17:08:08 -0300

    Update main.tf

--- sns-intro/main.tf
+++ sns-intro/main.tf
@@ -4,36 +4,15 @@ provider "aws" {
 
 # SNS Topic
 resource "aws_sns_topic" "notification_updates" {
-  name = "notification_updates"
+  name = "notification_updates-topic"
 }
 
 # SQS Queue - User Updates
 resource "aws_sqs_queue" "user_updates_queue" {
-  name   = "user-updates-queue"
-  policy = data.aws_iam_policy_document.user_updates_queue_policy.json
+  name = "user-updates-queue"
 }
 
-# Subscription SNS -> SQS (User Updates)
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  topic_arn = aws_sns_topic.notification_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.user_updates_queue.arn
-}
-
-# SQS Queue - Product Updates
-resource "aws_sqs_queue" "product_updates_queue" {
-  name   = "product-updates-queue"
-  policy = data.aws_iam_policy_document.product_updates_queue_policy.json
-}
-
-# Subscription SNS -> SQS (Product Updates)
-resource "aws_sns_topic_subscription" "product_updates_sqs_target" {
-  topic_arn = aws_sns_topic.notification_updates.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.product_updates_queue.arn
-}
-
-# IAM Policy para User Updates Queue
+# Policy para User Updates Queue
 data "aws_iam_policy_document" "user_updates_queue_policy" {
   statement {
     sid    = "Allow-SNS-SendMessage-UserUpdates"
@@ -55,7 +34,24 @@ data "aws_iam_policy_document" "user_updates_queue_policy" {
   }
 }
 
-# IAM Policy para Product Updates Queue
+resource "aws_sqs_queue_policy" "user_updates_queue_policy_attachment" {
+  queue_url = aws_sqs_queue.user_updates_queue.id
+  policy    = data.aws_iam_policy_document.user_updates_queue_policy.json
+}
+
+# Subscription SNS -> SQS (User Updates)
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.notification_updates.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.user_updates_queue.arn
+}
+
+# SQS Queue - Product Updates
+resource "aws_sqs_queue" "product_updates_queue" {
+  name = "product-updates-queue"
+}
+
+# Policy para Product Updates Queue
 data "aws_iam_policy_document" "product_updates_queue_policy" {
   statement {
     sid    = "Allow-SNS-SendMessage-ProductUpdates"
@@ -75,4 +71,16 @@ data "aws_iam_policy_document" "product_updates_queue_policy" {
       values   = [aws_sns_topic.notification_updates.arn]
     }
   }
+}
+
+resource "aws_sqs_queue_policy" "product_updates_queue_policy_attachment" {
+  queue_url = aws_sqs_queue.product_updates_queue.id
+  policy    = data.aws_iam_policy_document.product_updates_queue_policy.json
+}
+
+# Subscription SNS -> SQS (Product Updates)
+resource "aws_sns_topic_subscription" "product_updates_sqs_target" {
+  topic_arn = aws_sns_topic.notification_updates.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.product_updates_queue.arn
 }
